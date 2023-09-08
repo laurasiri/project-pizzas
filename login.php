5398c7236d79f3c0ae4c6d71e9cea49261a14d6e
laura siri <laurasolange09@gmail.com>
2023-09-08 15:33:49 -0300

    login

--- login.php
+++ login.php
@@ -1,7 +1,20 @@
 <?php
-$usr=$_POST["username"];
-$pass=$_POST["password"];
-if(((strcmp($usr,"maria")==0)||(strcmp($usr,"maria@twitter.com")==0))&&(strcmp($pass,"123456")==0))
-{echo"Bienvenidos";}
-else{echo"Denegado";}
+include_once("config_login.php");
+try{
+$pdo= new PDO("mysql:host=".SERVER_NAME .";dbname=".DATABASE_NAME,USER_NAME,PASSWORD);
+ // set the PDO error mode to exception
+ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+echo "conexion existosa";
+}
+catch(PDOException $e){
+    echo "conexion fallida". $e->getMessage();
+}
+$usr=$_POST['username'];
+$pass=$_POST['password'];
+$hashed_pass=hash('sha256',$pass);
+$sql="SELECT * from users WHERE ((username =:usr) or (email =:usr)) AND (PASSWORD =:hashed_pass) AND ( active ='si')";
+$stmt=$pdo->prepare($sql);
+$stmt->bindValve(':usr',$usr);
+$stmt->bindValve(':hashed_pass',$hashed_pass);
+$stmt->execute();
 ?>
